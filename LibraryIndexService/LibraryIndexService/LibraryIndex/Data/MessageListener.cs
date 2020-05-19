ff39d20de311de97bee5d7d364c68f9ae1efdb16
Kristofer Andersen <andersen19@gmail.com>
2020-05-19 16:38:07 +0200

    updated the structure
    added switch statements for the user choices

---
+++ LibraryIndexService/LibraryIndexService/LibraryIndex/Data/MessageListener.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.Azure.ServiceBus;
+using Microsoft.Azure.ServiceBus.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryIndex.Data
+{
+    public class MessageListener
+    {
+        // Connection String for the namespace can be obtained from the Azure portal under the 
+        // 'Shared Access policies' section.
+        const string ServiceBusConnectionString = "Endpoint=sb://libraryapi.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=krLVo6p2VJLKy/AQyVLlAMCMukWw3uxiNWgyiDeGivs=";
+        const string BasicQueueName = "librarybookqueue";
+
+        public async Task MainAsync()
+        {
+            await PeekMessagesAsync(ServiceBusConnectionString, BasicQueueName);
+        }
+
+        static async Task PeekMessagesAsync(string connectionString, string queueName)
+        {
+            var receiver = new MessageReceiver(connectionString, queueName, ReceiveMode.PeekLock);
+
+            Console.WriteLine("Browsing messages from Queue...");
+            while (true)
+            {
+                try
+                {
+                    // Browse messages from queue
+                    var message = await receiver.PeekAsync();
+                    // If the returned message value is null, we have reached the bottom of the log
+                    if (message != null)
+                    {
+                        // print the message
+                        var body = Encoding.UTF8.GetString(message.Body);
+                        lock (Console.Out)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Cyan;
+                            Console.WriteLine(
+                                "\t\t\t\tMessage peeked: \n\t\t\t\t\t\tMessageId = {0}, \n\t\t\t\t\t\tSequenceNumber = {1}, \n\t\t\t\t\t\tEnqueuedTimeUtc = {2}," +
+                                "\n\t\t\t\t\t\tExpiresAtUtc = {5}, \n\t\t\t\t\t\tContentType = \"{3}\", \n\t\t\t\t\t\tSize = {4}, \n\t\t\t\t\t\tState = {6}, " +
+                                "  \n\t\t\t\t\t\tContent: [ {7} ]",
+                                message.MessageId,
+                                message.SystemProperties.SequenceNumber,
+                                message.SystemProperties.EnqueuedTimeUtc,
+                                message.ContentType,
+                                message.Size,
+                                message.ExpiresAtUtc,
+                                "", //message.SystemProperties.State,// TODO: Need to restore that property
+                                body);
+                            Console.ResetColor();
+                        }
+                    }
+                    else
+                    {
+                        // We have reached the end of the log.
+                        break;
+                    }
+                }
+                catch (ServiceBusException e)
+                {
+                    if (!e.IsTransient)
+                    {
+                        Console.WriteLine(e.Message);
+                        throw;
+                    }
+                }
+            }
+            await receiver.CloseAsync();
+        }
+    }
+}
--- LibraryIndexService/LibraryIndexService/LibraryIndex/Program.cs
+++ LibraryIndexService/LibraryIndexService/LibraryIndex/Program.cs
@@ -1,82 +1,40 @@
-﻿using Microsoft.Azure.ServiceBus;
-using Microsoft.Azure.ServiceBus.Core;
-using Newtonsoft.Json;
+﻿using LibraryIndex.Data;
 using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace LibraryIndex
 {
     class Program
     {
-        // Connection String for the namespace can be obtained from the Azure portal under the 
-        // 'Shared Access policies' section.
-        const string ServiceBusConnectionString = "Endpoint=sb://libraryapi.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=krLVo6p2VJLKy/AQyVLlAMCMukWw3uxiNWgyiDeGivs=";
-        const string BasicQueueName = "librarybookqueue";
-
         static void Main(string[] args)
         {
-            MainAsync().GetAwaiter().GetResult();
-        }
-        static async Task MainAsync()
-        {
-            // broswe those messages 
-            await PeekMessagesAsync(ServiceBusConnectionString, BasicQueueName);
+            UserChoice();
         }
 
-       
-
-        static async Task PeekMessagesAsync(string connectionString, string queueName)
+        private static void UserChoice()
         {
-            var receiver = new MessageReceiver(connectionString, queueName, ReceiveMode.PeekLock);
+            Console.WriteLine("Library Indexer, browse the library.\n" +
+                        "\t 1. Get all the books. \n" +
+                        "\t 2. Something else.");
+            string choice = Console.ReadLine();
+            LibraryIndexPicker(choice);
+        }
 
-            Console.WriteLine("Browsing messages from Queue...");
-            while (true)
+        private static void LibraryIndexPicker(string choice)
+        {
+            MessageListener mListener = new MessageListener();
+            switch (choice)
             {
-                try
-                {
-                    // Browse messages from queue
-                    var message = await receiver.PeekAsync();
-                    // If the returned message value is null, we have reached the bottom of the log
-                    if (message != null)
-                    {
-                        // print the message
-                        var body = Encoding.UTF8.GetString(message.Body);
-                        lock (Console.Out)
-                        {
-                            Console.ForegroundColor = ConsoleColor.Cyan;
-                            Console.WriteLine(
-                                "\t\t\t\tMessage peeked: \n\t\t\t\t\t\tMessageId = {0}, \n\t\t\t\t\t\tSequenceNumber = {1}, \n\t\t\t\t\t\tEnqueuedTimeUtc = {2}," +
-                                "\n\t\t\t\t\t\tExpiresAtUtc = {5}, \n\t\t\t\t\t\tContentType = \"{3}\", \n\t\t\t\t\t\tSize = {4}, \n\t\t\t\t\t\tState = {6}, " +
-                                "  \n\t\t\t\t\t\tContent: [ {7} ]",
-                                message.MessageId,
-                                message.SystemProperties.SequenceNumber,
-                                message.SystemProperties.EnqueuedTimeUtc,
-                                message.ContentType,
-                                message.Size,
-                                message.ExpiresAtUtc,
-                                "", //message.SystemProperties.State,// TODO: Need to restore that property
-                                body);
-                            Console.ResetColor();
-                        }
-                    }
-                    else
-                    {
-                        // We have reached the end of the log.
-                        break;
-                    }
-                }
-                catch (ServiceBusException e)
-                {
-                    if (!e.IsTransient)
-                    {
-                        Console.WriteLine(e.Message);
-                        throw;
-                    }
-                }
+                case "1":
+                    Console.WriteLine("All the books available in the Library.");
+                    mListener.MainAsync().GetAwaiter().GetResult();
+                    break;
+                case "2":
+                    Console.WriteLine("Case 2");
+                    break;
+                default:
+                    Console.WriteLine("Default case");
+                    break;
             }
-            await receiver.CloseAsync();
         }
     }
 }
